f5fd1b74e9bc5c3ca7644a6516acab56b2a0b58e
MerleFriTH <merle.friedrichsen@th-wildau.de>
2018-03-15 23:20:41 +0100

    Kommentare

--- getMetadataOCLC.php
+++ getMetadataOCLC.php
@@ -1,13 +1,17 @@
 <?php
 error_reporting(E_ALL);
 /**
- * Created by IntelliJ IDEA.
- * User: Merle
+ * Author: Merle Friedrichsen
  * Date: 08.03.2018
- * Time: 22:22
  */
 require_once './My_MySQLi.php';
 
+/* function validateISBN
+ * param: string to validate
+ * return: validates ISBN or false
+ * validates serverinput, cleans data, checks for ISBN-pattern,
+ * removes "-" in ISBN, in order to store an isbn only once (not twice: with and without -)
+ */
 function validateISBN($data){
     $data = trim($data);
     $data = stripslashes($data);
@@ -21,6 +25,11 @@ function validateISBN($data){
     return $return;             
 }
 
+/* function askOCLC
+ * param: isbn
+ * return: response from OCLC
+ * sends request to OCLC, return metadata for ISBN  (if exisits)
+ */
 function askOCLC($isbn) {
     $format = "json";
     $method = "getMetadata";
@@ -36,6 +45,10 @@ function askOCLC($isbn) {
     return $result;
 }
 
+/*function sendResult
+ * param: ISBN
+ * return: retuns JSON encoded result from database
+ */
 function sendResult($inputisbn) {
     $mysql = new My_MySQLi("localhost", "root", "", "isbnMetadata");
     $selectSQL1 = "SELECT * FROM metadata WHERE isbn='$inputisbn'";
@@ -49,16 +62,21 @@ function sendResult($inputisbn) {
     return $json;
 }
 
+/*function getResults
+ * param: isbn
+ * return: return results from database, if no results in database,
+ * gets metadata from OCLC and stores in database
+ * always return the data from database -> so it has always the same parameters
+ *  */
 function getResults($inputisbn) {
     //openDB
     $mysql = new My_MySQLi("localhost", "root", "", "isbnMetadata");
     $selectSQL = "SELECT * FROM metadata WHERE isbn='$inputisbn'";
     $sqlResult = $mysql->query($selectSQL);
-    //echo var_dump($sqlResult);
     $numRows = mysqli_num_rows($sqlResult);
-    //echo $numRows;
     $returnvalue = "0";
-
+    // check if there are data in the sqlResult
+    //if not -> askOCLC
     if ($numRows == 0) {
         $result = askOCLC($inputisbn);
         //echo $result;
@@ -84,24 +102,25 @@ function getResults($inputisbn) {
             //open DB
             $insertSQL = "INSERT INTO metadata (publisher,lang,city,author,ed,year,isbn,title) VALUES ('$publisher','$lang','$city','$author','$ed','$year','$isbn','$title')";
             $mysql->query($insertSQL);
+            //get the metadata from the database
             $returnvalue = sendResult($inputisbn);
         }
-    } else {
+    } else {//if the data is alredy in the database, get it and return it
             $returnvalue = sendResult($inputisbn);
             //return $result;
     }
     return $returnvalue;
 }
 
-
+//get the data from the postrequest as json
 $content = trim(file_get_contents("php://input"));
 $decoded = json_decode($content, true);
 $userinput = $decoded["isbn"];
+//validate ISBN
 $valISBN = validateISBN($userinput);
 if ($valISBN != false) {
     echo getResults($valISBN);
 } else {
     echo "noResult";
 }
-//echo getResults("9787891231270");
 ?>
