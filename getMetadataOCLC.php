d2578556fb7bfd342b15698a238d7378d6301326
MerleFriTH <merle.friedrichsen@th-wildau.de>
2018-03-15 16:33:26 +0100

    Abfrage in DB funktioniert auch wenn kein Titel in DB vorhanden ist

--- getMetadataOCLC.php
+++ getMetadataOCLC.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(E_ALL);
 /**
  * Created by IntelliJ IDEA.
  * User: Merle
@@ -7,49 +8,47 @@
  */
 require_once './My_MySQLi.php';
 
-function askOCLC($isbn)
-{
+function askOCLC($isbn) {
     $format = "json";
     $method = "getMetadata";
 
-    $url = "http://xisbn.worldcat.org/webservices/xid/isbn/".$isbn."?method=".$method."&format=".$format."&fl=*";
+    $url = "http://xisbn.worldcat.org/webservices/xid/isbn/" . $isbn . "?method=" . $method . "&format=" . $format . "&fl=*";
     $result = file_get_contents($url);
-    
+
     $decode = json_decode($result);
-    
+
     if ($decode->stat == "ok") {
         return $result;
     } else {
         return "noResult";
     }
- 
 }
 
-function sendResult($inputisbn){
+function sendResult($inputisbn) {
     $mysql = new My_MySQLi("localhost", "root", "", "isbnMetadata");
     $selectSQL1 = "SELECT * FROM metadata WHERE isbn='$inputisbn'";
-    
+
     $resultDB = $mysql->query($selectSQL1);
 
-    while($row = $resultDB->fetch_array(MYSQL_ASSOC)) {
-            $myArray[] = $row;
+    while ($row = $resultDB->fetch_array(MYSQL_ASSOC)) {
+        $myArray[] = $row;
     }
     $json = json_encode($myArray);
     return $json;
-
 }
 
 function getResults($inputisbn) {
-    $result = "0";
     //openDB
     $mysql = new My_MySQLi("localhost", "root", "", "isbnMetadata");
     $selectSQL = "SELECT * FROM metadata WHERE isbn='$inputisbn'";
-    
-    if($result = $mysql->query($selectSQL)){
-        return sendResult($inputisbn);
-        //return $result;
-    } else {
+    $sqlResult = $mysql->query($selectSQL);
+    echo var_dump($sqlResult);
+    $numRows = mysqli_num_rows($sqlResult);
+    echo $numRows;
+
+    if ($numRows == 0) {
         $result = askOCLC($inputisbn);
+        echo $result;
         if ($result == "noResult") {
             return "noResult";
         } else {
@@ -59,25 +58,25 @@ function getResults($inputisbn) {
             $decoded = json_decode($result);
             $book = $decoded->list;
             $publisher = addslashes($book[0]->publisher);
-            $lang= addslashes($book[0]->lang);
-            $city= addslashes($book[0]->city);
-            $author= addslashes($book[0]->author);
-            $ed= addslashes($book[0]->ed);
-            $year= addslashes($book[0]->year);
-            $isbn= $book[0]->isbn;
+            $lang = addslashes($book[0]->lang);
+            $city = addslashes($book[0]->city);
+            $author = addslashes($book[0]->author);
+            $ed = addslashes($book[0]->ed);
+            $year = addslashes($book[0]->year);
+            $isbn = $book[0]->isbn;
             $isbn = addslashes($isbn[0]);
-            $title= addslashes($book[0]->title);
-            
+            $title = addslashes($book[0]->title);
+
             //open DB
             $insertSQL = "INSERT INTO metadata (publisher,lang,city,author,ed,year,isbn,title) VALUES ('$publisher','$lang','$city','$author','$ed','$year','$isbn','$title')";
             $mysql->query($insertSQL);
             return sendResult($inputisbn);
-            //return $result;
         }
+    } else {
+            return sendResult($inputisbn);
+            //return $result;
     }
 }
 
-echo getResults("389721105X");
-//getResults();
-
+echo getResults("3827315352");
 ?>
